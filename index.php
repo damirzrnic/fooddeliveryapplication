921422ac65eacce1e5f1a7fefa40fec45e628aee
damirzrnic <damir.zrnic@stu.ibu.edu.ba>
2022-03-27 13:10:57 -0500

    PDO code added

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 
 echo "I have no idea what I'm doing in this course.";
 
+$servername = "sql11.freemysqlhosting.net";
+$username = "sql11481792";
+$password = "B14PRvaN5L";
+$schema = "sql11481792";
 
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=schema", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 
 
 
