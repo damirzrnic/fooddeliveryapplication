4ee46c9b3d24bf5e695d274edb7b00e1bb0b7ec6
damirzrnic <damir.zrnic@stu.ibu.edu.ba>
2022-03-27 16:11:20 -0400

    PDO

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo "I have no idea what I'm doing in this course.";
 $servername = "localhost";
 $username = "damirzrnic";
 $password = "Hehexd123";
-$schema = "fooddeliveryapplication";
+$schema = "todo";
 
 
 try {
@@ -13,6 +13,9 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
+
+
+
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
