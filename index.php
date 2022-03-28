4e085bb1c0b9bc7576ff2cb79d36e63af23e2842
damir.zrnic <damir.zrnic@stu.ibu.edu.ba>
2022-03-27 20:15:32 -0500

    PDO another try

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ echo "I have no idea what I'm doing in this course./n";
 $servername = "127.0.0.1";
 $username = "damirzrnic";
 $password = "root";
-$schema = "fooddeliveryapp";
+
 
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$fooddeliveryapp", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
