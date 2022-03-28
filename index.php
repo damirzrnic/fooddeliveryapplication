1e6cdd7775c38781308c41c0947d57a78262cdfc
damir.zrnic <damir.zrnic@stu.ibu.edu.ba>
2022-03-27 20:25:11 -0500

    PDO still on square one

--- index.php
+++ index.php
@@ -9,13 +9,10 @@ $schema = "fooddeliveryapp";
 
 
 try {
-  $conn = new PDO("mysql:host=$servername;port=3306;dbname=$schema", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-
-
-
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
