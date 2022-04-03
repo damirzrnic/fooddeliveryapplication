e74f9bc81261023678353d3028414a430d576f30
damir.zrnic <damir.zrnic@stu.ibu.edu.ba>
2022-04-03 13:11:20 -0500

    PDO try 12

--- index.php
+++ index.php
@@ -2,17 +2,10 @@
 
 echo "I have no idea what I'm doing in this course";
 
-<<<<<<< HEAD
-$servername = "localhost";
-$username = "todo";
-$password = "todo";
-$schema = "todo";
-=======
 $servername = "sql11.freemysqlhosting.net";
 $username = "sql11481792";
 $password = "B14PRvaN5L";
 $schema = "sql11481792";
->>>>>>> 7200b08be6a978648b2aeba551d8e375edcc14b5
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
